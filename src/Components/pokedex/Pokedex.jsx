1a00ec02d62490eb17e416549e23b95259404930
Divanshu Kachhawa <divanshukachawa7878@gmail.com>
2023-12-20 14:56:34 +0530

    basic pokedex setup done 1

--- src/Components/pokedex/Pokedex.jsx
+++ src/Components/pokedex/Pokedex.jsx
@@ -4,11 +4,10 @@ import Search from "../Search/Search";
 function Pokedex() {
   return (
     <>
-      
       <div className="flex flex-col w-full items-center justify-center">
-      <h1 className="text-4xl font-serif font-bold tracking-widest bg-gradient-to-tr from-green-400 to-white bg-clip-text text-transparent">
-        Pokedex
-      </h1>
+        <h1 className="text-4xl font-serif font-bold tracking-widest bg-gradient-to-tr from-green-400 to-white bg-clip-text text-transparent">
+          Pokedex
+        </h1>
         <Search />
       </div>
 
