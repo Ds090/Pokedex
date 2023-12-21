ce25921ff1982927972acc0b1fb6d4972e79d0ca
Divanshu Kachhawa <divanshukachawa7878@gmail.com>
2023-12-21 15:39:19 +0530

    basic pokedex setup done 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
 import CustomRouts from "./routes/CustomRouter";
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <CustomRouts/>
+      <h1 className="text-4xl font-serif font-bold tracking-widest bg-gradient-to-tr from-green-400 to-white bg-clip-text text-transparent text-center">
+        <Link to='/' >Pokedex</Link>
+      </h1>
+      <CustomRouts />
     </>
   );
 }
--- src/Components/pokedex/Pokedex.jsx
+++ src/Components/pokedex/Pokedex.jsx
@@ -5,9 +5,7 @@ function Pokedex() {
   return (
     <>
       <div className="flex flex-col w-full items-center justify-center">
-        <h1 className="text-4xl font-serif font-bold tracking-widest bg-gradient-to-tr from-green-400 to-white bg-clip-text text-transparent">
-          Pokedex
-        </h1>
+        
         <Search />
       </div>
 
